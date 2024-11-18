56410c1942f339f49a2267edc1393316cde9fb3c
owasikohu <owatas2022@gmail.com>
2024-11-18 20:07:31 +0900

    add start.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt install ffmpeg git
+curl -fsSL https://bun.sh/install | bash
+bun install lowdb hono nodemailer fluent-ffmpeg
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+bun run --hot reien-BE/index.ts
+#bun run --hot reien-FE/index.ts
