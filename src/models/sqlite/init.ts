19619f19d54daa5637c25ebd356252c44836ffcd
owasikohu <owatas2022@gmail.com>
2025-03-02 23:26:50 +0900

    update sqlite/init.ts(create clients table)

--- src/models/sqlite/init.ts
+++ src/models/sqlite/init.ts
@@ -6,7 +6,7 @@ let db: Database;
 export async function init() {
     if (!db) {
     db = new Database(config.sqlite.path, { create: true });
-    db.query("CREATE TABLE IF NOT EXISTS clients(id TEXT, name TEXT, website TEXT, scopes TEXT, redirect_uri: TEXT)") //apps
+    db.query("CREATE TABLE IF NOT EXISTS clients(id TEXT PRIMARY KEY, client_id TEXT, client_secret TEXT, name TEXT, redirect_uri: TEXT, scopes TEXT, website TEXT, vapid_key TEXT, created_at TEXT)") //apps
     db.query("CREATE TABLE IF NOT EXISTS users(id TEXT PRIMARY KEY, created_at TEXT, updated_at TEXT, email TEXT, encrypted_password)").run();
     console.log("init sqlite");
     }
